22ae2d61f7a8f4b46eb209c4a3c2a77b92610f9f
Elhadji8-cyber <aladjikemod@gmail.com>
2026-06-09 14:28:57 -0700

    changement

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 export const env = {
   apiBaseUrl:
-    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api/v1",
-  wsUrl: process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8080/api/v1/ws",
+    process.env.NEXT_PUBLIC_API_BASE_URL || "https://gitbtp-server.onrender.com/api/v1",
+  wsUrl: process.env.NEXT_PUBLIC_WS_URL || "wss://gitbtp-server.onrender.com/api/v1/ws",
   devUserId: process.env.NEXT_PUBLIC_DEV_USER_ID || "",
   devCompanyId: process.env.NEXT_PUBLIC_DEV_COMPANY_ID || "",
   devAuthToken: process.env.NEXT_PUBLIC_DEV_AUTH_TOKEN || "",
